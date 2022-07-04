083ddd552589e94c1f1047fa1c092a2dc5d45d23
javadalam <javad.alamdar@smail.th-koeln.de>
2022-07-05 01:23:54 +0200

    update dococument

---
+++ v3/src/components/anmelden.vue
@@ -0,0 +1,67 @@
+<script setup>
+
+</script>
+
+<template>
+   
+      <div class=" flex-1 pt-8 bg-gray-200 text-gray-600">
+         <div class="login-main">
+           <div class="login-main-form">
+             <div class="font-extrabold px-20 text-xl">Create Your Account</div>
+               <form class="form" action="#" method="POST">
+                 <div>
+                    <label for="Email" class="form-lable"> Email Address </label>
+
+                    <div class="mt-2">
+                       <input
+                        id="email"
+                        name="email"
+                       type="email"
+                        autocomplete="email"
+                        required
+                        class="form-input"
+                       />
+                    </div>
+                </div>
+              <div>
+                <lable for="password" class="form-lable"> Password </lable>
+
+                <div class="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autocomplete="current-password"
+                    required
+                    class="form-input"
+                  />
+                </div>
+              </div>
+
+              <div class="flex items-center px-8">
+                          <input
+                              id="terms-and-privacy"
+                              name="terms-and-privacy"
+                              type="checkbox"
+                              class="rounded"
+                          />
+                          <label
+                             for="terms-and-privacy"
+                             class="ml-2 block text-sm text-gray-900"
+                             >I agree to the
+                             <a href="#" class="text-indigo-600 hover:text-indigo-500"
+                             >Terms</a>
+                           
+                             and
+                          <a href="#" class="text-indigo-600 hover:text-indigo-500"
+                           >Privacy Policy</a>
+                  
+                          </label>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+</template>
+
---
+++ v3/src/components/sidebar.vue
@@ -0,0 +1,61 @@
+<script>
+import anmelden from "./anmelden.vue"
+import IconCalender from "./icons/IconCalender.vue"
+import IconDashboard from "./icons/IconDashboard.vue"
+import IconHome from "./icons/IconHome.vue"
+import IconProject from "./icons/IconProject.vue"
+import IconTeam from "./icons/IconTeam.vue"
+import IconProject1 from "./icons/IconProject.vue"
+export default {
+    components:{
+        anmelden,
+        IconCalender,
+        IconDashboard,
+        IconHome,
+        IconTeam,
+        IconProject,
+        IconProject1,
+    }
+}
+</script>
+
+<template>
+   <div class="relative flex min-h-screen">
+    <div
+      class="bg-cyan-600 text-cyan-100 w-64 space-y-6 px-2 py-4"
+      
+    >
+      <a href="" class="flex items-center space-x-2 px-4">
+     <IconHome />
+        <span class="text-2xl font-extrabold text-white">Home</span>
+      </a>
+
+      <nav>
+        <a href="/" class="group flex items-center space-x-2 py-3 px-4 hover:bg-cyan-700 rounded hover:text-cyan-300 ">
+          <IconDashboard />
+          <span class="text-white group-hover:text-cyan-300">Dashbord</span>
+        </a>
+
+        <a href="/" class="group flex items-center space-x-2 py-3 px-4 hover:bg-cyan-700 rounded hover:text-cyan-300">
+          <IconTeam />
+          <span class="text-white group-hover:text-cyan-300">Team</span>
+        </a>
+        
+        <a href="/" class="group flex items-center space-x-2 py-3 px-4 hover:bg-cyan-700 rounded hover:text-cyan-300">
+         <IconProject />
+          <span class="text-white group-hover:text-cyan-300">Project</span>
+        </a>
+
+        <a href="/" class="group flex items-center space-x-2 py-3 px-4 hover:bg-cyan-700 rounded hover:text-cyan-300 transition duration-300">
+         <IconCalender />
+         <span class="text-white group-hover:text-cyan-300">Calender</span>
+        </a>
+
+      </nav>
+    </div>
+    <anmelden />
+  </div>
+    
+  
+</template>
+
